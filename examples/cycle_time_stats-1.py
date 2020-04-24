37e77bd00b0c9547a09ff73a5c75034a5870de8a
runarut <runarut@users.noreply.github.com>
2020-04-24 14:03:00 -0500

    added documentation. added sample code to examples.

---
+++ examples/cycle_time_stats-1.py
@@ -0,0 +1,24 @@
+# Version 1
+from statistics import mean, median, stdev, quantiles
+from ios_sensor_pack.models import Location
+locations = Location.objects.all().order_by('id')
+old_timestamp = None
+time_difference_list = []
+for location in locations:
+  if old_timestamp:
+    time_difference = location.sat_timestamp - old_timestamp
+    time_difference_seconds = time_difference.total_seconds()
+    time_difference_list.append(time_difference_seconds)
+
+  old_timestamp = location.sat_timestamp
+
+print("min: %s\nmax:%s\nmean: %s\nmedian: %s\nstdev: %s\nquantiles: %s" % (
+      str(min(time_difference_list))
+      str(max(time_difference_list))
+      str(mean(time_difference_list)),
+      str(median(time_difference_list)),
+      str(stdev(time_difference_list)),
+      str(quantiles(time_difference_list)),
+    )
+  )
+
